cd03c79c0c5614c9154fd1668a942c3604b3ca39
Zachary Lasky <zachary.lasky@W-DH-ML-ZLASKY2.local>
2021-07-18 15:18:50 -0400

    update copy

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,14 +1,12 @@
-import * as styles from "../styles";
-import * as assets from "../assets";
+import * as styles from '../styles';
+import * as assets from '../assets';
 
 export const missionContent = {
   text: {
     first:
-      "I am a full stack software engineer. I have written for production codebases in JavaScript, Golang, and Ruby on Rails.",
-    second:
-      "Most of the time, I write front end code using React and TypeScript.",
-    third:
-      "On the back end, I will most often write code using Node with Express."
+      'I am a full stack software engineer. I have written for production codebases in JavaScript, Golang, and Ruby on Rails.',
+    second: 'Most of the time, I write front end code using React and TypeScript.',
+    third: 'On the back end, I will most often write code using Node with Express.'
   },
   textSectionColor: styles.red,
   image: assets.interview
@@ -17,11 +15,11 @@ export const missionContent = {
 export const purposeContent = {
   text: {
     first:
-      "A few years ago I made the transition from working in music to being a software engineer.",
+      'A few years ago I made the transition from working in music to being a software engineer.',
     second:
-      "After I graduated college with a bachelors degree in music business, I attended Lambda School in to help me achieve my goal of learning to code.",
+      'After I graduated college with a bachelors degree in music business, I attended Lambda School in to help me achieve my goal of learning to code.',
     third:
-      "Now I've been worked in Boston, MA as a software engineer for a startup, Talla and currently for a much larger company, Healthgrades."
+      "Now I've worked in Boston, MA as a software engineer for a startup, Talla and currently for a much larger company, Healthgrades."
   },
   textSectionColor: styles.green,
   image: assets.lightbulb
@@ -29,9 +27,8 @@ export const purposeContent = {
 
 export const promiseContent = {
   text: {
-    first: "I like taking on new projects or hearing about software news.",
-    second:
-      "I'm also available for consulting or mentoring other who want to get into my field.",
+    first: 'I like taking on new projects or hearing about software news.',
+    second: "I'm also available for consulting or mentoring other who want to get into my field.",
     third: "If you'd like to connect, send me and email or add me on LinkedIn."
   },
   textSectionColor: styles.yellow,
